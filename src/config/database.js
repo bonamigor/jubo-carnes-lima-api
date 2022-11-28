418c4bc585f52edbdd10db0c841c69de34adfc23
Rafael Ribeiro Bonamigo <rafaelrbonamigo@gmail.com>
2022-11-27 23:42:04 -0300

    Added 'port' attribute to mysql connection.

--- src/config/database.js
+++ src/config/database.js
@@ -8,6 +8,7 @@ try {
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT,
     connectionLimit: 10,
     queueLimit: 80,
   });
