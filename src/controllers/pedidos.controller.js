c59527eb8cdbf885778f28901464e7ce611cfa82
Rafael Bonamigo <rafaelrbonamigo@gmail.com>
2023-03-07 10:22:50 -0300

    Ajuste para relatorio trazer apenas pedidos confirmados/entregues

--- src/controllers/pedidos.controller.js
+++ src/controllers/pedidos.controller.js
@@ -394,7 +394,10 @@ exports.ordersByClientReport = async (req, res) => {
       SELECT pedidos.id AS id, clientes.nome AS cliente, pedidos.data_criacao AS dataCriacao, pedidos.data_entrega AS dataEntrega, pedidos.valor_total AS total 
       FROM pedidos 
       INNER JOIN clientes ON clientes.id = pedidos.cliente_id
-      WHERE pedidos.cliente_id = ? AND valor_total > 0 AND pedidos.data_entrega BETWEEN ? AND ? 
+      WHERE pedidos.cliente_id = ? 
+      AND valor_total > 0 
+      AND pedidos.data_entrega BETWEEN ? AND ?  
+      AND pedidos.status = "CONFIRMADO" OR pedidos.status = "ENTREGUE";
     `;
 
     db.execute(selectQuery, [clienteId, dataInicial, dataFinal], (err, results) => {
